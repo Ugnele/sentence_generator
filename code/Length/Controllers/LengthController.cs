0888be61fdae0ed857f07b25d82bd1422fb30500
Ugnele <ugne.sarakauskaite@gmail.com>
2021-06-09 10:07:36 +0100

    Implemented a service (together with unit tests) generating random length of the sentence (in scope 1 to 5)

--- code/Length/Controllers/LengthController.cs
+++ code/Length/Controllers/LengthController.cs
@@ -11,6 +11,13 @@ namespace Length.Controllers
     [Route("[controller]")]
     public class LengthController : ControllerBase
     {
-        
+
+        [HttpGet]
+        public int GetSentenceLength()
+        {
+            var random = new Random();
+            int sentenceLength = random.Next(1, 5);
+            return sentenceLength;
+        }
     }
 }
---
+++ code/SentenceGeneratorTest/Utility/LengthControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using Length.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SentenceGeneratorTest.Utility
+{
+    public class LengthControllerTest
+    {
+        [Fact]
+        public void IsInteger()
+        {
+            LengthController length = new();
+
+            var testIsInteger = length.GetSentenceLength();
+            Assert.IsType<int>(testIsInteger);
+        }
+
+        [Fact]
+        public void IsInScope()
+        {
+            LengthController length = new();
+
+            for(int i = 0; i < 1000; i++)
+            {
+                var testIsInScope = length.GetSentenceLength();
+                Assert.IsType<int>(testIsInScope);
+                Assert.True(testIsInScope <= 5);
+                Assert.True(testIsInScope >= 1);
+            }
+        }
+    }
+}
