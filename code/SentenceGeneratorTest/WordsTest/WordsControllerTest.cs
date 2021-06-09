6668455e7042439f729e6fc272d5e74afb5b69ec
Ugnele <ugne.sarakauskaite@gmail.com>
2021-06-09 15:03:20 +0100

    Separated tests in sentenceGeneratorTest project in order to have more structured code. Added first test for WordsController which checks if the path for word files is obtained correctly

---
+++ code/SentenceGeneratorTest/WordsTest/WordsControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Words.Controllers;
+using Xunit;
+
+namespace SentenceGeneratorTest.WordsTest
+{
+    public class WordsControllerTest
+    {
+        [Fact]
+        public void GetResourcesPathTest()
+        {
+            WordsController words = new();
+            string parentPath = Path.GetFullPath(Path.Combine(words.GetResourcesPath(), @"..\"));
+            Assert.True(Directory.Exists(parentPath));
+        }
+
+        /// Checks if 
+        /*[Fact]
+        public void ReadFileTest()
+        {
+            WordsController words = new();
+            Assert.NotNull(words.ReadFile("verbs"));
+        }
+        */
+    }
+}
--- code/Words/Controllers/WordsController.cs
+++ code/Words/Controllers/WordsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,18 @@ namespace Words.Controllers
     [Route("[controller]")]
     public class WordsController : ControllerBase
     {
-        
+        public string GetResourcesPath()
+        {
+            string path = Directory.GetCurrentDirectory();
+            string parentPath = Path.GetFullPath(Path.Combine(path, @"..\..\..\"));
+            string resourcesPath = Path.GetFullPath(Path.Combine(parentPath, @"resources\"));
+            
+            return resourcesPath;
+        }
+        public string[] ReadFile(string path)
+        {
+            string[] words = System.IO.File.ReadAllLines($@"\Recourses\{path}.txt");
+            return words;
+        }
     }
 }
