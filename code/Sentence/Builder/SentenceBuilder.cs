b0b8fb18959dbf4a7dcd1175ed107639beb1c63e
Ugnele <ugne.sarakauskaite@gmail.com>
2021-06-10 20:53:10 +0100

    In sentence project added SentenceBuilder class in order to restore original data structure of words stored in Words project. Implemented StoreWords method together with tests for it

---
+++ code/Sentence/Builder/SentenceBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sentence.Builder
+{
+    public class SentenceBuilder
+    {
+        public Dictionary<string, List<string>> StoreWords(string words)
+        {
+            char[] c = words.ToCharArray();
+            string key = "";
+            Dictionary<string, List<string>> wordsDic = new();
+            List<string> wordList = new();
+
+            string word = "";
+            int cIndex = 0;
+            while (cIndex < c.Length)
+            {
+                if (Char.IsLetter(c[cIndex]) && c[cIndex] != ' ')
+                {
+                    word += c[cIndex];
+                } else
+                {
+                    while ((cIndex + 1) < c.Length 
+                        && !Char.IsLetter(c[cIndex + 1])) cIndex++;
+
+                    if (word.Equals("adjectives") || word.Equals("adverbs") ||
+                        word.Equals("nouns") || word.Equals("verbs"))
+                    {
+                        key = word;
+                    } else
+                    {
+                        if(wordList.Count > 1)
+                        {
+                            wordsDic.Add("nouns", wordList);
+                            wordList.Clear();
+                        }
+                        wordList.Add(word);
+                        if (!key.Equals("nouns") && !key.Equals(""))
+                        {
+                            wordsDic.Add(key, wordList);
+                            wordList.Clear();
+                        }
+                    }
+                    word = "";
+                }
+                cIndex++;
+            }
+            return wordsDic;
+        }
+
+        
+     }
+}
---
+++ code/SentenceGeneratorTest/SentenceTest/SentenceBuilderTest.cs
@@ -0,0 +1,34 @@
+﻿using Sentence.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SentenceGeneratorTest.SentenceTest
+{
+    public class SentenceBuilderTest
+    {
+        private string wordsDummy = "{\"adjectives\": [ \"bombproof\" ], " +
+                "\"adverbs\": [ \"astronomically\" ], \"nouns\": [ \"Platycerium\", \"mead\" ]," +
+                "\"verbs\": [ \"develop\" ]}";
+        [Fact]
+        public void IsNotNull()
+        {
+            SentenceBuilder sb = new();
+            Assert.NotNull(sb.StoreWords(wordsDummy));
+        }
+
+        [Fact]
+        public void StoresKeys()
+        {
+            SentenceBuilder sb = new();
+            Dictionary<string, List<string>> wordsDic = sb.StoreWords(wordsDummy);
+
+            Assert.Equal(4, wordsDic.Keys.Count);
+            Assert.Equal("adjectives", wordsDic.Keys.First().ToString());
+            Assert.Equal("verbs", wordsDic.Keys.Last().ToString());
+        }
+    }
+}
