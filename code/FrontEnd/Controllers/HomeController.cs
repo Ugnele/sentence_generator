a0feffca52a4d856feffa93ff5bb287cf90a3df8
Ugnele <ugne.sarakauskaite@gmail.com>
2021-06-16 12:41:00 +0100

    eliminated hard-coding from home and sentence controllers

--- code/FrontEnd/Controllers/HomeController.cs
+++ code/FrontEnd/Controllers/HomeController.cs
@@ -7,14 +7,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FrontEnd.Models;
 using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace FrontEnd.Controllers
 {
     public class HomeController : Controller
     {
+        private IConfiguration Configuration;
+
+        public HomeController(IConfiguration config)
+        {
+            Configuration = config;
+        }
         public async Task<IActionResult> Index()
         {
-            var sentenceService = "https://localhost:44366/sentence";
+            //var sentenceService = "https://localhost:44366/sentence";
+            var sentenceService = $"{Configuration["sentenceServiceURL"]}";
             var sentenceResponceCall = await new HttpClient().GetStringAsync(sentenceService);
             ViewBag.responseCall = sentenceResponceCall;
             return View();
