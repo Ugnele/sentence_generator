9a0e50a502b7f35ce9327c807c3e80dd226a47df
Ugnele <ugne.sarakauskaite@gmail.com>
2021-06-22 11:29:21 +0100

    updated secret values in appsettings of webapp3, also, the values now are not exposed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,13 +10,6 @@ variable "rg_name" {
   default = "ugnesarakauskaite"
 }
 
-variable "appservice" {
-  default = {
-    location            = "uksouth"
-    resource_group_name = "ugnesarakauskaite"
-  }
-}
-
 variable "resources" {
   default = {
     "adjectives" = 1
@@ -26,17 +19,12 @@ variable "resources" {
   }
 }
 
-variable "configuration3" {
-  default = {
-    "StorageConnectionString" = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=ugnestorageaccount;AccountKey=402fSd5BmMpkDz4L+TwUlrlPdAH2nECFP/FC6WBOMl+ff3NVVES1TvW2nNcCHR+1s1B/+lsRsrNdmKDCNeqMtw=="
-    "StorageAccountName"      = "ugnestorageaccount"
-    "StorageAccountKey"       = "402fSd5BmMpkDz4L+TwUlrlPdAH2nECFP/FC6WBOMl+ff3NVVES1TvW2nNcCHR+1s1B/+lsRsrNdmKDCNeqMtw=="
-  }
+variable "StorageConnectionString" {
+  sensitive = true
+  type      = string
 }
 
-variable "configuration4" {
-  default = {
-    "lengthServiceURL" = "https://ugne-webapp2.azurewebsites.net/length"
-    "wordsServiceURL"  = "https://ugne-webapp3.azurewebsites.net/words"
-  }
+variable "StorageAccountKey" {
+  sensitive = true
+  type      = string
 }
